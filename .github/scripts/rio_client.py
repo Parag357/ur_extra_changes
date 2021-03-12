81106e6a2e325ed2de3d65221d2ec129f9a90363
Parag Bhattacharjee <parag.bhattacharjee@rapyuta-robotics.com>
2021-03-12 18:44:39 +0530

    trying to use secrets

--- .github/scripts/rio_client.py
+++ .github/scripts/rio_client.py
@@ -1,15 +1,14 @@
-import yaml
+import os
 from rapyuta_io import Client
 
 
 def get_rio_client():
     client = None
-    try:
-        with open('.github/app_config/rio-auth.yaml', 'r') as read_file:
-            auth_details = yaml.load(read_file, Loader=yaml.FullLoader)
-        client = Client(auth_token=auth_details['auth_token'], project=auth_details['project_id'])
-    except IOError:
-        print("File containing auth details is not accessible.")
+    auth_token = os.getenv('AUTH_TOKEN', default=None)
+    project_id = os.getenv('PROJECT_ID', default=None)
+    if not (auth_token and project_id):
+        return client
+    client = Client(auth_token=auth_token, project=project_id)
     return client
 
 
