3ce8a36f1b9b6a50a68c5844e2e47035fd416b01
Parag Bhattacharjee <parag.bhattacharjee@rapyuta-robotics.com>
2021-04-22 10:23:34 +0530

    modifying sim

--- .github/scripts/create_build.py
+++ .github/scripts/create_build.py
@@ -22,7 +22,7 @@ rosDistro = None
 isRos = create_build_dict['isRos']
 
 if isRos:
-    simulationOptions = SimulationOptions(False)
+    simulationOptions = SimulationOptions(create_build_dict['SimulationOption'])
     buildOptions = BuildOptions(catkinOptions=[CatkinOption()])
     rosDistro = create_build_dict['rosDistro']
 
