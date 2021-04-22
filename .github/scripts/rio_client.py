86a951801bd6d8eab029449a5485e4df28f14825
Parag Bhattacharjee <parag.bhattacharjee@rapyuta-robotics.com>
2021-04-22 10:46:50 +0530

    using github secrets

--- .github/scripts/rio_client.py
+++ .github/scripts/rio_client.py
@@ -4,8 +4,8 @@ from rapyuta_io import Client
 
 def get_rio_client():
     client = None
-    auth_token = 'GvAHnGNU9JuRPY9WKPGt4BYMtzDxXUMosjufp3z7'  # os.getenv('AUTH_TOKEN', default=None)
-    project_id = 'project-hkbdxfszjqilfbjyyngmuxij'  # os.getenv('PROJECT_ID', default=None)
+    auth_token = os.getenv('AUTH_TOKEN', default=None)
+    project_id = os.getenv('PROJECT_ID', default=None)
     if not (auth_token and project_id):
         return client
     client = Client(auth_token=auth_token, project=project_id)
